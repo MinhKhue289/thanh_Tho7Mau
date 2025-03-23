70d3203fe632dea1c75204cb21261c08b53668f3
MinhKhue289 <eoviti06@gmail.com>
2025-03-23 14:36:32 +0700

    First commit

---
+++ source/script.js
@@ -0,0 +1,11 @@
+document.getElementById("products").addEventListener("click",function() {
+    document.getElementById("products").scrollIntoView({
+        behavior: "smooth"  
+    })
+});
+
+document.getElementById("client").addEventListener("click",function() {
+    document.getElementById("client").scrollIntoView( {
+        behavior: "smooth"
+    })
+});
